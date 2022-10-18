7fcecc002584deec1f3ad0b87cd43d18b442b274
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-18 15:37:11 +0530

    Prob6 Compute Quotient And Remainder

---
+++ Quotient and Remainder/Quotient and Remainder/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Quotient_and_Remainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividend = 50, divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            
+        }
+    }
+}

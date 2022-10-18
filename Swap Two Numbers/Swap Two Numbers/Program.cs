0d0e8e35a37f1da2a5348debdb739675f173e5d8
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-18 15:40:46 +0530

    Prob7 Swap Two Number

---
+++ Swap Two Numbers/Swap Two Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Swap_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Emter the second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}

33e8b5b22d84e92edbbf18cd84df9734c94e77d1
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-18 15:32:05 +0530

    Prob3 Power of Two

---
+++ Power of 2/Power of 2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Power_of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            //Console.WriteLine("Hello, World!");
+            Console.Write("Input a number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            double end = Math.Pow(2, number);
+            for (int i = 1; i < number; i++)
+            {
+                double sqr = Math.Pow(i, 2);
+                if(sqr < end)
+                {
+
+                    Console.WriteLine(sqr);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}

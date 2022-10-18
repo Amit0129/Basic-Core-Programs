9f5059a3b3da25e7f6068975b9474a04fd941e78
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-18 15:42:57 +0530

    Prob8 Number Is Even Or Odd

---
+++ Number is Even or Odd/Number is Even or Odd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Number_is_Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num%2==0)
+            {
+                Console.WriteLine("The number " + num + " is even number");
+            }
+            else
+            {
+                Console.WriteLine("The number " + num + " is odd number");
+            }
+        }
+    }
+}

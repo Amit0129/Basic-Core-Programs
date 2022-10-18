25f5d2a6be345e318c11be03678c1e43cf3e2973
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-18 15:28:13 +0530

    Prob2 Print the Year is a Leap Year or Not

---
+++ Leap Year/Leap Year/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Leap_Year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chk_year;
+            //Console.WriteLine("Hello, World!");
+            Console.Write("Input an year : ");
+            chk_year = Convert.ToInt32(Console.ReadLine());
+
+            if ((chk_year % 400) == 0)
+                Console.WriteLine("{0} is a leap year.\n", chk_year);
+            else if ((chk_year % 100) == 0)
+                Console.WriteLine("{0} is not a leap year.\n", chk_year);
+            else if ((chk_year % 4) == 0)
+                Console.WriteLine("{0} is a leap year.\n", chk_year);
+            else
+                Console.WriteLine("{0} is not a leap year.\n", chk_year);
+        }
+    }
+}

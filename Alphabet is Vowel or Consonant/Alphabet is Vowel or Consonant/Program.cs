590e151277cbb38973639d4d19f2d3d51e8509e4
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-18 15:45:58 +0530

    Prob9 Vowel Or Consonant

---
+++ Alphabet is Vowel or Consonant/Alphabet is Vowel or Consonant/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Alphabet_is_Vowel_or_Consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            letter = Convert.ToChar(Console.ReadLine().ToLower());
+            switch (letter)
+            {
+                case 'a':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("The Alphabet is not a vowel");
+                    break;
+            }
+
+
+
+        }
+    }
+}
